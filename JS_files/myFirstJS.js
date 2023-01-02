a6f7c05f844a36a0b88701d839ce6e6c26e4ba9e
Brandon Biernat <brandon.biernat@students.pcci.edu>
2023-01-02 12:30:23 -0500

    Started learning JavaScript and adding functionality to the page

---
+++ JS_files/myFirstJS.js
@@ -0,0 +1,13 @@
+
+
+function fillCircle() {
+    document.getElementById("circle").src="../Images/FullCircle.png";
+}
+
+function emptyCircle() {
+    document.getElementById("circle").src="../Images/EmptyCircle.png";
+}
+
+function changeText() {
+    document.getElementById("jsText").style.fontSize="35px";
+}
